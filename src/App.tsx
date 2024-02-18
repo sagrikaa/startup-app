7d968d52205a9d455b64a44e7f8f39b0ca77cb29
Sagrika Aggarwal <sagrikaaggarwal@Sagrikas-MacBook-Pro.local>
2024-02-17 19:27:04 -0500

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Typography } from '@mui/material';
 
 function App() {
   return (
@@ -8,7 +13,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <Typography variant="h4" component="h1" align="left">
+            Your Journery to enabling online growth starts here
+          </Typography>
         </p>
         <a
           className="App-link"
